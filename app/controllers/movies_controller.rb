c30fb0d1e0c843deeb908a8f7a9a44fe0a0e6acc
Rohan Sood <rohansood@berkeley.edu>
2020-10-20 23:23:38 +0000

    added functionality

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,10 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    
+    @all_ratings = Movie.all_ratings
+    @ratings_to_show = Movie.ratings_to_show(params[:ratings])
+    @movies = Movie.with_ratings(@ratings_to_show)
   end
 
   def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,23 @@
 class Movie < ActiveRecord::Base
+  def self.all_ratings
+    ['G','PG','PG-13','R']
+  end
+  
+  def self.ratings_to_show(checkbox_ratings)
+    if checkbox_ratings != nil
+      checkbox_ratings.keys
+    else
+      []
+    end
+  end
+  def self.with_ratings(ratings_list)
+    if ratings_list != nil
+      Movie.where(rating: ratings_list)
+    else
+      Movie
+    end
+  # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all
+  #  movies with those ratings
+  # if ratings_list is nil, retrieve ALL movies
+  end
 end
